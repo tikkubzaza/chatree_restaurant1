76b162b8735a461613c36efdf624b2f1e92a4c98
tikkubzaza <ooolooo1150@gmail.com>
2016-10-26 11:16:48 +0700

    เทส

--- app/src/main/java/com/tikkubzaza/chatree_restaurant/OrderTable.java
+++ app/src/main/java/com/tikkubzaza/chatree_restaurant/OrderTable.java
@@ -1,5 +1,6 @@
 package com.tikkubzaza.chatree_restaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,10 +11,25 @@ public class OrderTable {
     private MySQLiteOpenHelper objMySQLiteOpenHelper;
     private SQLiteDatabase writeSqliteDatabase, readSqLiteDatabase;
 
+    public static final String ORDER_TABLE = "orderTABLE";
+    public static final String COLUMN_ID_ORDER = "_id";
+    public static final String COLUMN_OFFICER = "Officer";
+    public static final String COLUMN_DESK = "desk";
+    public static final String COLUMN_FOOD = "food";
+    public static final String COLUMN_ITEM = "Item";
+
     public OrderTable(Context context){
         objMySQLiteOpenHelper = new MySQLiteOpenHelper(context);
         writeSqliteDatabase = objMySQLiteOpenHelper.getWritableDatabase();
         readSqLiteDatabase = objMySQLiteOpenHelper.getReadableDatabase();
+    }
+    public long addOrder (String strOfficer, String strDesk, String strFood, String strItem){
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_OFFICER,strOfficer);
+        objContentValues.put(COLUMN_DESK,strDesk);
+        objContentValues.put(COLUMN_FOOD,strFood);
+        objContentValues.put(COLUMN_ITEM,strItem);
+        return readSqLiteDatabase.insert(ORDER_TABLE,null,objContentValues);
     }
 }
 
--- app/src/main/java/com/tikkubzaza/chatree_restaurant/UserTable.java
+++ app/src/main/java/com/tikkubzaza/chatree_restaurant/UserTable.java
@@ -10,9 +10,22 @@ public class UserTable {
     private MySQLiteOpenHelper objMySQLiteOpenHelper;
     private SQLiteDatabase writeSqliteDatabase, readSqLiteDatabase;
 
+    public static final String USER_TABLE = "userTABLE";
+    public static final String COLUMN_ID_USER = "_id";
+    public static final String COLUMN_USER = "User";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_NAME = "Name";
+
     public UserTable(Context context){
         objMySQLiteOpenHelper = new MySQLiteOpenHelper(context);
         writeSqliteDatabase = objMySQLiteOpenHelper.getWritableDatabase();
         readSqLiteDatabase = objMySQLiteOpenHelper.getReadableDatabase();
     }
+    public long addOrder (String strOfficer, String strDesk, String strFood, String strItem){
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_OFFICER,strOfficer);
+        objContentValues.put(COLUMN_DESK,strDesk);
+        objContentValues.put(COLUMN_FOOD,strFood);
+        objContentValues.put(COLUMN_ITEM,strItem);
+        return readSqLiteDatabase.insert(ORDER_TABLE,null,objContentValues);
 }
